63a8a87ba24b1f4eed91c1fff41f54a1efa7f719
DVMMY <damola-adeniyi@hotmail.co.uk>
2018-11-01 19:50:19 +0000

    deploying build version

---
+++ precache-manifest.84d2fe0ed17fe318a684041f4cc6e6ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a58c43d64bb7cdeebae0",
+    "url": "/static/js/main.a58c43d6.chunk.js"
+  },
+  {
+    "revision": "4ef173bc222c877b4549",
+    "url": "/static/js/1.4ef173bc.chunk.js"
+  },
+  {
+    "revision": "a58c43d64bb7cdeebae0",
+    "url": "/static/css/main.1cf74e3f.chunk.css"
+  },
+  {
+    "revision": "01de11109a408e4b082a7307271ee0de",
+    "url": "/index.html"
+  }
+];
